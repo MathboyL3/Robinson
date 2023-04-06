bf4797a46a820ecc608e2326358e3c46b4ff8db4
MathboyL3 <63455990+MathboyL3@users.noreply.github.com>
2023-04-06 14:39:45 -0300

    Added 2 methods & changes to file writing
    2 methods was added to JSONmaster.cs, ConvertToJson and ConvertFromJson.
    Changed the way it used to write text to files. Instead of using a StreamWriter, now it uses File.WriteAllText method.
    I used to verify if the save directory exists befores trying to create the directory, but thre's no need to verify it beforehand because the method will only create directories that doesn't exist yet.

--- JsonMaster/JSONmaster.cs
+++ JsonMaster/JSONmaster.cs
@@ -5,17 +5,15 @@ namespace MyLib.JSONFile
     public class JSON
     {
         static JsonSerializerOptions default_option = new JsonSerializerOptions { WriteIndented = true };
-        public static bool Save<T>(T object_to_save, string directory, string file_name)
+        public static bool Save(object object_to_save, string directory, string file_name)
         {
             try
             {
-                if(!Directory.Exists(directory))
-                    Directory.CreateDirectory(directory);
+                Directory.CreateDirectory(directory);
 
                 string file_path = $"{directory}\\{file_name}.json";
                 string json_string = JsonSerializer.Serialize(object_to_save, default_option);
-                using StreamWriter sw = new StreamWriter(file_path);
-                sw.WriteLine(json_string);
+                File.WriteAllText(file_path, json_string);
                 return true;
             }
             catch(Exception ex)
@@ -24,6 +22,16 @@ namespace MyLib.JSONFile
                 Console.WriteLine(ex.ToString());
                 return false;
             }
+        }
+
+        public static string ConvertoToJson(object obj_to_convert)
+        {
+            return JsonSerializer.Serialize(obj_to_convert, default_option);
+        }
+
+        public static T ConvertoFromJson<T>(string obj_to_convert)
+        {
+            return JsonSerializer.Deserialize<T>(obj_to_convert, default_option);
         }
 
         public static T Load<T>(string full_path) {
--- JsonMaster/Program.cs
+++ JsonMaster/Program.cs
@@ -5,13 +5,21 @@ public static class Program
     //Testing
     public static void Main()
     {
-        Aluno saved_aluno = new Aluno("Daniel", "696969", 9, 2);
+        List<Aluno> alunos = new List<Aluno>()
+        {
+            new Aluno("Daniel", "450678"),
+            new Aluno("Náthan", "456845"),
+            new Aluno("Ryan", "978789"),
+        };
 
-        JSON.Save(saved_aluno, ".\\Saves", "aluno");
+        JSON.Save(alunos, ".\\Saves", "aluno");
 
-        Aluno loaded_aluno = JSON.Load<Aluno>(".\\Saves\\aluno.json");
+        List<Aluno> loaded_alunos = JSON.Load<List<Aluno>>(".\\Saves\\aluno.json");
 
-        loaded_aluno.Print();
+        foreach(Aluno a in loaded_alunos)
+            a.Print();
+
+        Console.WriteLine(JSON.ConvertoToJson(loaded_alunos));
         Console.ReadKey();
     }
 }
